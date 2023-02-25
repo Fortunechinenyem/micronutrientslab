5bf15dd10a19a578047e646739fe0d0b769ea14e
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-02-25 03:34:13 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,69 +26,75 @@ const Footer = () => {
   return (
     <section className="footer">
       <Logo className="mb-5" />
-      <div className="d-flex container">
-        <div className="left">
-          <h3>Contact Us</h3>
-          <h4>
-            <FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }} />
-            +234(802) 4430 439 +234(802) 4430 439
-          </h4>
-          <h4 className="email">
-            <FaMailBulk
-              size={20}
-              style={{ color: "#fff", marginRight: "2rem" }}
-            />
-            mudassar@gmail.com
-          </h4>
+      <div className="container">
+        <div className="d-flex  row ">
+          <div className="left col-md">
+            <h3>Contact Us</h3>
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              +234(802) 4430 439 +234(802) 4430 439
+            </h4>
+            <h4 className="email">
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              mudassar@gmail.com
+            </h4>
 
-          <div className="location">
-            <GoLocation
-              size={20}
-              style={{ color: "#fff", marginRight: "2rem" }}
-            />
-            Plot C39A, Amuwo Odofin Commerical Scheme, Along Alakoso Avenue,
-            Amuwo Odofin, Lagos, Nigeria
+            <div className="location">
+              <GoLocation
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              Plot C39A, Amuwo Odofin Commerical Scheme, Along Alakoso Avenue,
+              Amuwo Odofin, Lagos, Nigeria
+            </div>
           </div>
-        </div>
-        <div className="center">
-          <h3>Our Services</h3>
-          <div>
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
-            <Link className="nav-link" to="/about">
-              About Us
-            </Link>
-            <Link className="nav-link" to="/product">
-              Our Product
-            </Link>
-            <Link className="nav-link" to="/about">
-              Blog
-            </Link>
-            <Link className="nav-link" to="/about">
-              Get Cognito
-            </Link>
-          </div>
+          <div className="center col-md">
+            <h3>Our Services</h3>
+            <div>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/about">
+                About Us
+              </Link>
+              <Link className="nav-link" to="/product">
+                Our Product
+              </Link>
+              <Link className="nav-link" to="/about">
+                Blog
+              </Link>
+              <Link className="nav-link" to="/about">
+                Get Cognito
+              </Link>
+            </div>
 
-          <div className="right">
-            <div className="social">
-              <FaFacebook
-                size={20}
-                style={{ color: "#fff", marginRight: "2rem" }}
-              />
-              <FaLinkedin
-                size={20}
-                style={{ color: "#fff", marginRight: "2rem" }}
-              />
+            <div className="right ">
+              <div className="social col-md">
+                <FaFacebook
+                  size={20}
+                  style={{ color: "#fff", marginRight: "2rem" }}
+                />
+                <FaLinkedin
+                  size={20}
+                  style={{ color: "#fff", marginRight: "2rem" }}
+                />
 
-              <FaTwitter
-                size={20}
-                style={{ color: "#fff", marginRight: "2rem" }}
-              />
+                <FaTwitter
+                  size={20}
+                  style={{ color: "#fff", marginRight: "2rem" }}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <p className="mt-5 mb-1">
         ​Copyright © 2023, Fortune.dev. All Rights Reserved by{" "}
         <span className="herotext1">MICRONUTRIENT LAB</span>
