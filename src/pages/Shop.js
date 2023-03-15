4b3d02f6658be6e5cd7762fba783ba7b66a2be32
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-03-15 03:48:12 +0100

    update

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,8 +2,6 @@ import Footer from "../components/Footer";
 import Shop1 from "../components/Shop1";
 import Shophero from "../components/Shophero";
 
-import Cart from "./Cart";
-
 const Shop = () => {
   return (
     <>
